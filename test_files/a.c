db25f2b35c1ba9b7977a65406af3680c837f228f
Shengchen Kan <shengchen.kan@intel.com>
2025-07-30 00:49:25 +0800

    [test] Pre-commit tests for bb2fline
    addr2line use debug_info first then symbol table, which causes the
    inlined function’s name to be used, e.g.
    
    ```
    $ addr2line -e ./test_files/a.out -f 0x123f
    typo
    /home/skan/sim_utils/./test_files/a.c:6
    ```
    
    We can find the function `typo` is never called but it appears in
    a.err.f.csv, which is a problem.

--- test_files/a.c
+++ test_files/a.c
@@ -2,13 +2,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+float __attribute__((always_inline)) typo(int r) {
+  asm volatile (
+      "jmp 1f\n"
+      "nop\n"
+      "1:"
+  );
+  return r * 3.14i * 2;
+}
+
 int main() {
   srand(time(NULL));
   int i = 0;
   while (i++ < 1000) {
     int r = rand();
     printf("%d\n", r);
-    float x = r * 3.14i * 2;
+    float x = typo(r);
     printf("%f\n", x);
   }
 
